86462042aa325c41b1659cdfcbae7e98742ba2c5
Lhenri10 <152184654+Lhenri10@users.noreply.github.com>
2023-11-27 10:46:31 -0300

    Cadastro de pessoas(exercicios js)

---
+++ script.js
@@ -0,0 +1,11 @@
+// Definindo variáveis com exemplos
+var nome = "João Silva";
+var salario = 50000.5; // Exemplo de salário com valor decimal
+var idade = 25;
+var possuiDiploma = true;
+
+// Exibindo informações no console
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui Diploma: " + possuiDiploma);
